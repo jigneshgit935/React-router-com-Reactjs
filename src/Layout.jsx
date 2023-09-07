f869b092e51c76eca4dcf907e36c89f4bc4aa25f
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-08 04:25:09 +0530

    router-updated

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,16 +1,15 @@
-import './App.css';
+import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import Home from './components/Home/Home';
 
-function App() {
+const Layout = () => {
   return (
     <>
       <Header />
-      <Home />
+      <Outlet />
       <Footer />
     </>
   );
-}
+};
 
-export default App;
+export default Layout;
